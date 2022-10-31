13bdba7689216cb3eaaecae528a1658845687878
lanthora <lanthora@outlook.com>
2022-10-31 02:48:05 +0000

    [openssl] 添加 OpenSSL 探针

---
+++ probe/src/main.rs
@@ -0,0 +1,60 @@
+use aya::programs::UProbe;
+use aya::{include_bytes_aligned, Bpf};
+use clap::Parser;
+use log::{info, LevelFilter};
+use tokio::signal;
+
+#[derive(Debug, Parser)]
+struct Opt {
+    #[clap(short, long)]
+    pid: Option<i32>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let opt = Opt::parse();
+
+    env_logger::builder()
+        .format_timestamp(None)
+        .filter_level(LevelFilter::Info)
+        .init();
+
+    #[cfg(debug_assertions)]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/debug/probe"
+    ))?;
+    #[cfg(not(debug_assertions))]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/release/probe"
+    ))?;
+
+    // TODO: 扫描 /proc/ 目录中的所有进程, 并从进程对应的 maps 里拿到 libssl
+    // 的绝对路径, 最后拼成 /proc/pid/root/path/to/libssl.so.x.x 的形式.
+    // 通过循环对每个有效的进程设置以下 4 个 hook 函数.
+    let target = "libssl";
+    let fn_name = "uprobe_enter_openssl_write";
+    let program: &mut UProbe = bpf.program_mut(fn_name).unwrap().try_into()?;
+    program.load()?;
+    program.attach(Some("SSL_write"), 0, target, opt.pid.try_into()?)?;
+
+    let fn_name = "uprobe_exit_openssl_write";
+    let program: &mut UProbe = bpf.program_mut(fn_name).unwrap().try_into()?;
+    program.load()?;
+    program.attach(Some("SSL_write"), 0, target, opt.pid.try_into()?)?;
+
+    let fn_name = "uprobe_enter_openssl_read";
+    let program: &mut UProbe = bpf.program_mut(fn_name).unwrap().try_into()?;
+    program.load()?;
+    program.attach(Some("SSL_read"), 0, target, opt.pid.try_into()?)?;
+
+    let fn_name = "uprobe_exit_openssl_read";
+    let program: &mut UProbe = bpf.program_mut(fn_name).unwrap().try_into()?;
+    program.load()?;
+    program.attach(Some("SSL_read"), 0, target, opt.pid.try_into()?)?;
+
+    info!("Waiting for Ctrl-C...");
+    signal::ctrl_c().await?;
+    info!("Exiting...");
+
+    Ok(())
+}
